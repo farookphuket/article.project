4d394fb2700fbab29dae0a4839824345a903ce33
farook <farookphuket@gmail.com>
2020-09-23 06:50:47 +0700

    laptop update RAW

---
+++ resources/views/INC/body_import_js.blade.php
@@ -0,0 +1,20 @@
+<!-- 
+    DO NOT import JQUERY! it is oready in the <head> import ! 
+    
+        - need html syntax <script src="{{asset("path/to/js-file")}}"
+-->
+
+
+
+
+<!-- prism -->
+
+
+
+
+
+<!-- hljs -->
+
+
+
+<!-- jodit -->
